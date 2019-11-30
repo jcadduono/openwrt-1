9f3974c81cc3c721053dc3083c19abdc38594c92
Oever González <notengobattery@gmail.com>
2019-11-29 20:59:35 -0600

    luci-app-compressed-memory: add new package
    This commit adds support for the compressed-memory package. This is a
    package designed to manage the compressed memory in the system. This
    allows the system to save memory and reduce I/O to swap devices. By
    enabling this LuCI application, users can customize basic features of the
    compressed memory subsystem. More features and fine tunning may be
    archieved by using the UCI interface.
    
    Signed-off-by: Oever González <notengobattery@gmail.com>

---
+++ feeds/luci/applications/luci-app-compressed-memory/luasrc/controller/admin/compressed_memory.lua
@@ -0,0 +1,12 @@
+--
+-- Copyright (C) 2019 Oever González <notengobattery@gmail.com>
+--
+-- Licensed to the public under the Apache License 2.0.
+--
+
+module("luci.controller.admin.compressed_memory", package.seeall)
+
+function index()
+	entry({"admin", "system", "compressed_memory"}, view("compressed_memory"), _("Compressed memory"), 51)
+		.file_depends = { "/etc/config/compressed_memory" }
+end
